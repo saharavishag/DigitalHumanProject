d39f12a89208832eab964254a5419d0626a7edb6
Avishag Sahar <avsahar@cisco.com>
2020-01-16 09:03:50 +0200

    politicians

--- NewsApi/GetNews.py
+++ NewsApi/GetNews.py
@@ -2,6 +2,14 @@ from newsapi import NewsApiClient
 import json
 import os
 
+politicians = [['בינימין נתניהו','ביבי','נתניהו','בינימין נתניהו','ראש הממשלה','בנימין נתניהו'],
+               ['בני גנץ','גנץ','בני גנץ','בנימין גנץ','בינימין גנץ'],
+               ['אביגדור ליברמן','אביגדור איווט ליברמן','ליברמן'],
+               [],
+               [],
+               [],
+               [],]
+
 api_key = os.environ.get('api_key')
 
 newsapi = NewsApiClient(api_key=api_key)
