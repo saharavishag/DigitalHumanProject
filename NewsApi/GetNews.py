3c84f2e436300e67dc183508544e8d566d729798
Avishag Sahar <avsahar@cisco.com>
2020-02-14 15:52:21 +0200

    update request

--- NewsApi/GetNews.py
+++ NewsApi/GetNews.py
@@ -2,28 +2,33 @@ from newsapi import NewsApiClient
 import json
 import os
 
-politicians = [['בינימין נתניהו','ביבי','נתניהו','בינימין נתניהו','ראש הממשלה','בנימין נתניהו'],
-               ['בני גנץ','גנץ','בני גנץ','בנימין גנץ','בינימין גנץ'],
-               ['אביגדור ליברמן','אביגדור איווט ליברמן','ליברמן'],
-               ['גבי אשכנזי'],
-               ['יאיר לפיד'],
-               ['עמיר פרץ'],
-               ['סתיו שפיר'],
-               ['גדעון סער'],
-               ['מירי רגב'],
-               ['איילת שקד'],
-               ['נפתלי בנט'],
-               ['טיבי'],
-               ['רפי פרץ'],]
+politicians = {"bibi": ['בינימין נתניהו','ביבי','נתניהו','בינימין נתניהו','ראש הממשלה','בנימין נתניהו'],
+               "gantz": ['בני גנץ','גנץ','בני גנץ','בנימין גנץ','בינימין גנץ'],
+               "liberman": ['אביגדור ליברמן','אביגדור איווט ליברמן','ליברמן'],
+               "gabi": ['גבי אשכנזי'],
+               "lapid": ['יאיר לפיד'],
+               "amir": ['עמיר פרץ'],
+               "stav": ['סתיו שפיר'],
+               "gidon": ['גדעון סער'],
+               "miri": ['מירי רגב'],
+               "shaked": ['איילת שקד'],
+               "benet": ['נפתלי בנט'],
+               "tibi": ['טיבי', 'אחמד טיבי'],
+               "rafi": ['רפי פרץ'],}
 
 api_key = os.environ.get('api_key')
 
 newsapi = NewsApiClient(api_key=api_key)
 
-with open('content/json/newsdata.json', 'w', encoding='utf-8') as f:
-    with open('content/text/newsdata.txt', 'w', encoding='utf-8') as t:
-        all_articles = newsapi.get_everything(q='ביבי')
-        json.dump(all_articles, f)
-        print(all_articles, file=t)
+for p in politicians:
+    for p1 in politicians[p]:
+        os.makedirs(os.path.dirname('content/json/{p}/newsdata.json'.format(p=p)), exist_ok=True)
+        os.makedirs(os.path.dirname('content/text/{p}/newsdata.txt'.format(p=p)), exist_ok=True)
+        with open('content/json/{p}/newsdata.json'.format(p=p), 'a+', encoding='utf-8') as f:
+            with open('content/text/{p}/newsdata.txt'.format(p=p), 'a+', encoding='utf-8') as t:
+                print(p1)
+                all_articles = newsapi.get_everything(q=p1)
+                json.dump(all_articles, f)
+                print(all_articles, file=t)
 
 # print(json.dumps(all_articles))
