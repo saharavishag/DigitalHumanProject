5928a84e792ad209aee9036b6c267e655078ef02
Avishag Sahar <avsahar@cisco.com>
2020-02-16 16:34:12 +0200

    update format

--- NewsApi/GetNews.py
+++ NewsApi/GetNews.py
@@ -21,14 +21,16 @@ api_key = os.environ.get('api_key')
 newsapi = NewsApiClient(api_key=api_key)
 
 for p in politicians:
+    i = 0
     for p1 in politicians[p]:
-        os.makedirs(os.path.dirname('content/json/{p}/newsdata.json'.format(p=p)), exist_ok=True)
-        os.makedirs(os.path.dirname('content/text/{p}/newsdata.txt'.format(p=p)), exist_ok=True)
-        with open('content/json/{p}/newsdata.json'.format(p=p), 'a+', encoding='utf-8') as f:
-            with open('content/text/{p}/newsdata.txt'.format(p=p), 'a+', encoding='utf-8') as t:
+        os.makedirs(os.path.dirname('content/json/{p}/{i}newsdata.json'.format(p=p, i=i)), exist_ok=True)
+        os.makedirs(os.path.dirname('content/text/{p}/{i}newsdata.txt'.format(p=p, i=i)), exist_ok=True)
+        with open('content/json/{p}/{i}newsdata.json'.format(p=p, i=i), 'a+', encoding='utf-8') as f:
+            with open('content/text/{p}/{i}newsdata.txt'.format(p=p, i=i), 'a+', encoding='utf-8') as t:
                 print(p1)
                 all_articles = newsapi.get_everything(q=p1)
                 json.dump(all_articles, f)
                 print(all_articles, file=t)
+                i = i + 1
 
 # print(json.dumps(all_articles))
