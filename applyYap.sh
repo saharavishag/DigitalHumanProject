9d77f7eff620598713ee751d468b457727f91e8b
Avishag Sahar <avsahar@cisco.com>
2020-02-18 19:00:50 +0200

    fix applyYap

--- applyYap.sh
+++ applyYap.sh
@@ -3,19 +3,40 @@
 for d in $(ls /Users/avsahar/school/Digitalhuman/DigitalHumanProject/tokens/); 
 do 
     echo $d;
+    INPUTDIR="/Users/avsahar/school/Digitalhuman/DigitalHumanProject/tokens/$d/"
+    LATTICEDIR="/Users/avsahar/school/Digitalhuman/DigitalHumanProject/lattice/$d/"
+    FINALRESDIR="/Users/avsahar/school/Digitalhuman/DigitalHumanProject/finalresults/$d/"
+    mkdir -p $LATTICEDIR
+    mkdir -p $FINALRESDIR
     for f in $(ls /Users/avsahar/school/Digitalhuman/DigitalHumanProject/tokens/$d);
         do
             echo $f;
-            INPUTFILE="/Users/avsahar/school/Digitalhuman/DigitalHumanProject/tokens/$d/$f"
+            INPUTFILE="$INPUTDIR$f"
             echo $INPUTFILE;
-            LATTICEFILE="/Users/avsahar/school/Digitalhuman/DigitalHumanProject/lattice/$d/$f.lattice"
+            LATTICEFILE="$LATTICEDIR$f.lattice"
             echo $LATTICEFILE;
             ../../../go/src/yap/yap hebma -raw $INPUTFILE -out $LATTICEFILE
-            OUTPUTFILESEG="/Users/avsahar/school/Digitalhuman/DigitalHumanProject/finalresults/$d/$f.segmentation"
-            OUTPUTFILEMAP="/Users/avsahar/school/Digitalhuman/DigitalHumanProject/finalresults/$d/$f.mapping"
-            OUTPUTFILECON="/Users/avsahar/school/Digitalhuman/DigitalHumanProject/finalresults/$d/$f.conll"
+            OUTPUTFILESEG="$FINALRESDIR$f.segmentation"
+            OUTPUTFILEMAP="$FINALRESDIR$f.mapping"
+            OUTPUTFILECON="$FINALRESDIR$f.conll"
             ../../../go/src/yap/yap joint -in $LATTICEFILE -os $OUTPUTFILESEG -om $OUTPUTFILEMAP -oc $OUTPUTFILECON
         done
 
 done
+
+# LATTICEDIR="/Users/avsahar/school/Digitalhuman/DigitalHumanProject/lattice/amir/"
+# FINALRESDIR="/Users/avsahar/school/Digitalhuman/DigitalHumanProject/finalresults/amir/"
+# mkdir -p $LATTICEDIR
+# mkdir -p $FINALRESDIR
+
+# INPUTFILE="/Users/avsahar/school/Digitalhuman/DigitalHumanProject/tokens/amir/0.0tokens.txt"
+# echo $INPUTFILE;
+# cat $INPUTFILE;
+# LATTICEFILE="/Users/avsahar/school/Digitalhuman/DigitalHumanProject/lattice/amir/0.0tokens.txt.lattice"
+# echo $LATTICEFILE;
+# ../../../go/src/yap/yap hebma -raw $INPUTFILE -out $LATTICEFILE
+# OUTPUTFILESEG="/Users/avsahar/school/Digitalhuman/DigitalHumanProject/finalresults/amir/0.0tokens.txt.segmentation"
+# OUTPUTFILEMAP="/Users/avsahar/school/Digitalhuman/DigitalHumanProject/finalresults/amir/0.0tokens.txt.mapping"
+# OUTPUTFILECON="/Users/avsahar/school/Digitalhuman/DigitalHumanProject/finalresults/amir/0.0tokens.txt.conll"
+# ../../../go/src/yap/yap joint -in $LATTICEFILE -os $OUTPUTFILESEG -om $OUTPUTFILEMAP -oc $OUTPUTFILECON
 
